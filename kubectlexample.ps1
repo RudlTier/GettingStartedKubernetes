be6c04e1b41cf2745ec932958759c9528f0d19b2
Michael Rudl <michael.rudl@gmx.de>
2021-04-22 12:38:31 +0200

    init commit

---
+++ kubectlexample.ps1
@@ -0,0 +1,29 @@
+
+# Create Deployment from .yaml file (-f for passing yaml file)
+kubectl create -f .\yaml\webserver.yaml
+# If deployed again: Error from server (AlreadyExists): error when creating "webserver.yaml": deployments.apps "webserver" already exists
+
+# Create NodePort Service which opens a static port on all worker nodes which proxies to the dedicated service
+kubectl create -f .\yaml\webserver-svc.yaml
+
+# A more direct method of creating a Service is by exposing the previously created Deployment (this method requires an existing Deployment).
+# Expose a Deployment with the kubectl expose command:
+kubectl expose deployment webserver --name=web-service --type=NodePort
+
+# It is not necessary to create the Deployment first, and the Service after. They can be created in any order. A Service will find and connect Pods based on the Selector.
+
+###### GET Requests
+# Get all pods in current namespace
+kubectl get pods
+
+# Get all pods with label app=nginx
+kubectl get pods -l app=nginx 
+
+# Get all pods with label app=nginx and list value for this label
+kubectl get pods -L app -l app=nginx
+
+# Get all replica sets in current namespace
+kubectl get replicasets
+
+# List Services 
+kubectl get services
---
+++ minikubeexample.ps1
@@ -0,0 +1,9 @@
+
+# Open Minikube dashboard
+minikube dashboard
+
+# Get Ip of minikube vm
+minikube ip
+
+# Open service in web browser
+minikube service web-service
